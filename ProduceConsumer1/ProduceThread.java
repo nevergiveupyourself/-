42202bfa7638cdb12e13807308919a17e70d33b2
xiexudong <xie417184376@hotmail.com>
2019-09-26 16:39:54 +0800

    Create ProduceThread.java

---
+++ ProduceConsumer1/ProduceThread.java
@@ -0,0 +1,16 @@
+package ProduceConsumer1;
+
+public class ProduceThread extends Thread{
+    private Produce p;
+    public ProduceThread(Produce p){
+        this.p=p;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            p.produce();
+        }
+//        p.produce();
+    }
+}
